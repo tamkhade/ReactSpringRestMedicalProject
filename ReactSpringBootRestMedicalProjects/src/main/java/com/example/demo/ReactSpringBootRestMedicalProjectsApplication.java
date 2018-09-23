d3a7a62750d0b03eccf7ab5f27a543b2fe85332e
tamkhade21 <tamkhade21@gmail.com>
2018-09-23 10:44:28 +0530

    create repository for medical project

---
+++ ReactSpringBootRestMedicalProjects/src/main/java/com/example/demo/ReactSpringBootRestMedicalProjectsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootRestMedicalProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootRestMedicalProjectsApplication.class, args);
+	}
+}
