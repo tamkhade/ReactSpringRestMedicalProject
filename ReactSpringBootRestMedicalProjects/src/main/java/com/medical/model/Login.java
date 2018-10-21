7b861cf4fd49644ce005239bf105137c12642cec
tamkhade21 <tamkhade21@gmail.com>
2018-10-21 15:57:59 +0530

    Adding signup service and changes in model

--- ReactSpringBootRestMedicalProjects/src/main/java/com/medical/model/Login.java
+++ ReactSpringBootRestMedicalProjects/src/main/java/com/medical/model/Login.java
@@ -8,17 +8,22 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name ="Login")
+@Table(name ="Signup")
 public class Login {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column (name = "id")
 	private int id;
 	
+	@Column(name="firstname")
+	private String firstname;
+	
+	@Column(name="lastname")
+	private String lastname;
+	
 	@Column (name = "userName")
 	private String userName;
 	@Column (name = "password")
-
 	private String password;
 	
 	public int getId() {
@@ -39,5 +44,19 @@ public class Login {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
 
 }
